5be9f19b95a287adbf8a88e049ee8e5eee4d55ad
EomLions <eomlions@gmail.com>
2025-01-14 20:43:38 -0500

    Robot oriented drive

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -79,6 +79,9 @@ public final class Constants {
   public static final class OIConstants {
     public static final int kDriverControllerPort = 0;
     public static final double kDriveDeadband = 0.05;
+    public static final int kDriverControllerXAxis = 0;
+    public static final int kDriverControllerYAxis = 1;
+    public static final int kDriverControllerRotAxis = 4;
   }
 
   public static final class AutoConstants {
