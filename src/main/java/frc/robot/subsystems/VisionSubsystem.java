ca8356379de56cd5215474b2951a37e8228b756d
EomLions <eomlions@gmail.com>
2025-01-21 20:48:10 -0500

    [TESTED - Elliott] Network table code works with response from limelight

--- src/main/java/frc/robot/subsystems/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/VisionSubsystem.java
@@ -6,7 +6,10 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.LimelightHelpers;
 import frc.robot.Constants.LimelightConstants;
 
 public class VisionSubsystem extends SubsystemBase {
@@ -15,12 +18,26 @@ public class VisionSubsystem extends SubsystemBase {
 
   /** Creates a new VisionSubsystem. */
   public VisionSubsystem() {
+    // Send data to Python
+    double[] dataToSend = { ((Constants.LimelightConstants.hasBorders) ? 1 : 0) };
+    LimelightHelpers.setPythonScriptData("", dataToSend);
     networkTable = NetworkTableInstance.getDefault().getTable(LimelightConstants.kNetworkTableKey);
+
   }
 
   @Override
   public void periodic() {
+
     pythonEntry = networkTable.getEntry(LimelightConstants.kNetworkTableEntry).getDoubleArray(new double[] {});
+
+    // SmartDashboard.putNumber("Limelight x", pythonEntry[0]);
+    // SmartDashboard.putNumber("Limelight y", pythonEntry[1]);
+    // SmartDashboard.putNumber("Limelight w", pythonEntry[2]);
+    // SmartDashboard.putNumber("Limelight h", pythonEntry[3]);
+    SmartDashboard.putNumber("Limelight distance", pythonEntry[0]);
+    SmartDashboard.putNumber("Limelight H-angle", pythonEntry[0]);
+    SmartDashboard.putNumber("Limelight V-angle", pythonEntry[0]);
+
     // This method will be called once per scheduler run
   }
 }
