531ec0c800f7431fac8a793506128bb4c7beed51
Elliott <starlliott@gmail.com>
2025-01-26 14:24:38 -0500

    Merged vision into drivetrain branch

--- src/main/java/frc/robot/limelight/LimelightHelpers.java
+++ src/main/java/frc/robot/limelight/LimelightHelpers.java
@@ -1,12 +1,10 @@
-package frc.robot;
+package frc.robot.limelight;
 
 import edu.wpi.first.networktables.DoubleArrayEntry;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.TimestampedDoubleArray;
-import frc.robot.LimelightHelpers.LimelightResults;
-import frc.robot.LimelightHelpers.PoseEstimate;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
--- src/main/java/frc/robot/subsystems/vision/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/vision/LimelightSubsystem.java
@@ -2,22 +2,22 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems;
+package frc.robot.subsystems.vision;
 
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import frc.robot.LimelightHelpers;
 import frc.robot.Constants.LimelightConstants;
+import frc.robot.limelight.LimelightHelpers;
 
-public class VisionSubsystem extends SubsystemBase {
+public class LimelightSubsystem extends SubsystemBase {
   private final NetworkTable networkTable;
   private double[] pythonEntry = {};
 
   /** Creates a new VisionSubsystem. */
-  public VisionSubsystem() {
+  public LimelightSubsystem() {
     // Send data to Python
     double[] dataToSend = { ((Constants.LimelightConstants.hasBorders) ? 2 : 1) }; // 2 is true 1 is false
     LimelightHelpers.setPythonScriptData("", dataToSend);
