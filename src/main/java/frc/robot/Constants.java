5c7f267883483e09edf26d874a8ec979510c5e1c
RyanLin5967 <idident5967@gmail.com>
2025-02-01 16:11:22 -0500

    feb 1

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -141,10 +141,10 @@ public final class Constants {
     public static final int kTopShooterMotorPort = 0;
     public static final int kBottomShooterMotorPort = 0;
 
-    public static final double kPShooterController = 0.000025;
-    public static final double kIShooterController = 0.0;// 000000012;
+    public static final double kPShooterController = 0.000058;
+    public static final double kIShooterController = 0.0;
     public static final double kDShooterController = 0;
-    public static final double kShooterVelocityFF = 0;
+    public static final double kShooterVelocityFF = 0.00021;
   }
 
   // PDP CAN IDs
