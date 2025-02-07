6b2d0c92692eeed547fa52a43271cd19e885451e
Wilson <wilsonyu657@gmail.com>
2025-02-06 22:48:58 -0500

    Reorganized batch file

--- vision-startup.bat
+++ vision-startup.bat
@@ -1,22 +1,33 @@
 @REM Environment variables
-set "USER=pi"
-set "ADDRESS=pi@10.178.28.1"
-set "PASSWORD=raspberry"
-set "REMOTE_DIR=/home/%USER%/2025_7476_reefscape"
-set "PY_SCRIPT=limelight"
+@set "USER=pi"
+@set "ADDRESS=pi@10.178.28.1"
+@set "PASSWORD=raspberry"
+@set "REMOTE_DIR=/home/%USER%/2025_7476_reefscape"
+@set "PY_SCRIPT=limelight"
 
 @REM Pull from GitHub (subject to change)
-git pull
+@echo Attempt to pull from GitHub (if possible)
+@git pull
 
 @REM Store credentials
-cmdkey /generic:%ADDRESS% /user:%USER% /pass:%PASSWORD%
+@echo Store SSH credentials
+@cmdkey /generic:%ADDRESS% /user:%USER% /pass:%PASSWORD%
 
 @REM Create directory on the pi
-ssh %ADDRESS% "rm -rf %REMOTE_DIR% && mkdir -p %REMOTE_DIR%"
+@echo Create directory onto the pi
+@ssh %ADDRESS% "rm -rf %REMOTE_DIR% && mkdir -p %REMOTE_DIR%" || goto end
 
 @REM Clone python module onto pi
-scp -r %PY_SCRIPT% %ADDRESS%:%REMOTE_DIR%
+@echo Clone python module through SSH
+@scp -r %PY_SCRIPT% %ADDRESS%:%REMOTE_DIR% || goto end
 
-ssh %ADDRESS% "cd %REMOTE_DIR%/%PY_SCRIPT% && pip install -r requirements.txt && python3 -m python.algae.algaeClassifcation"
+@REM Install requirements and run code
+@echo Install PIP requirements and run module
+@ssh %ADDRESS% "cd %REMOTE_DIR%/%PY_SCRIPT% && pip install -r requirements.txt && python3 -m python.algae.algaeClassifcation" || goto end
+
+:end
 
 pause
+
+exit 0
+
