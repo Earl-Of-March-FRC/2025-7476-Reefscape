23428984582642fe1f2a78f7443e694aadd1153f
Wilson <wilsonyu657@gmail.com>
2025-02-06 20:34:39 -0500

    Add theoretical startup batch for vision

---
+++ vision-startup.bat
@@ -0,0 +1,21 @@
+@REM Environment variables
+set "USER=pi"
+set "REMOTE_DIR=/home/pi/2025_7476_reefscape"
+set "PY_SCRIPT=limelight"
+set "ADDRESS=pi@photonvision.local"
+
+@REM Pull from GitHub (subject to change)
+git pull
+
+@REM @REM Install missing dependencies (subject to change)
+@REM pip install -r limelight\requirements.txt
+
+@REM Create directory on the pi
+ssh %ADDRESS% "rm -rf %REMOTE_DIR% && mkdir -p %REMOTE_DIR%"
+
+@REM Clone python module onto pi
+scp -r %PY_SCRIPT% %ADDRESS%:%REMOTE_DIR%
+
+ssh %ADDRESS% "cd %REMOTE_DIR%/%PY_SCRIPT% && python3 -m pip install -r requirements.txt && python3 -m python.algae.algaeClassifcation"
+
+pause
