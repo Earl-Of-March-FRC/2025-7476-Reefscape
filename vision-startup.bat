7ce8d939202ed74d7f0aa40840b21a87710de4c3
wyu4 <wilsonyu657@gmail.com>
2025-02-08 22:39:11 -0500

    Use PuTTY for ssh protocols
    Attempt to use PuTTY packages to perform SSH jobs in order to automate the password field. Will revert to using default PowerShell commands if that fails.

--- vision-startup.bat
+++ vision-startup.bat
@@ -1,34 +1,86 @@
+@echo off
+
 @REM Environment variables
 @set "USER=pi"
-@set "ADDRESS=pi@10.74.76.22"
+@set "ADDRESS=%USER%@10.74.76.22"
 @set "PASSWORD=raspberry"
 @set "REMOTE_DIR=/home/%USER%/2025_7476_reefscape"
 @set "PY_DIRECTORY=limelight"
 
-@REM @REM Pull from GitHub (subject to change)
-@REM @echo Attempt to pull from GitHub (if possible)
-@REM @git pull
-
-@REM Store SSH credentials
-@echo Storing SSH credentials
-@cmdkey /generic:%ADDRESS% /user:%USER% /pass:%PASSWORD%
-
+@REM --------------------------------------------------------------------------------------------
 @REM Create directory on the Pi
-@echo Creating directory on the Pi
-@ssh %ADDRESS% "rm -rf %REMOTE_DIR% && mkdir -p %REMOTE_DIR%" || goto end
+@echo [STEP 1] Creating directory on the Pi...
+@echo.
 
+:putty_create_dir
+plink -ssh %ADDRESS% -pw %PASSWORD% "rm -rf %REMOTE_DIR% && mkdir -p %REMOTE_DIR%" || (
+    call :putty_fail 
+    goto ssh_create_dir
+)
+goto putty_clone_py
+
+:ssh_create_dir
+ssh %ADDRESS% "rm -rf %REMOTE_DIR% && mkdir -p %REMOTE_DIR%" || goto default_fail
+goto putty_clone_py
+
+@REM --------------------------------------------------------------------------------------------
 @REM Clone the Python module onto the Pi
-@echo Cloning Python module through SSH
-@scp -r %PY_DIRECTORY% %ADDRESS%:%REMOTE_DIR% || goto end
+@echo [STEP 2] Cloning Python module through SSH...
+@echo.
 
-@REM @REM Install requirements and run the module using -m
-@REM @echo Installing PIP requirements and running the module
-@REM @ssh %ADDRESS% "cd %REMOTE_DIR%/%PY_DIRECTORY% && pip install -r requirements.txt && python3 -m python.algae.algaeClassification" || goto end
+:putty_clone_py
+pscp -pw %PASSWORD% -r %PY_DIRECTORY% %ADDRESS%:%REMOTE_DIR% || (
+    call :putty_fail 
+    goto scp_clone_py
+)
+goto putty_setup_startup
 
+:scp_clone_py
+scp -r %PY_DIRECTORY% %ADDRESS%:%REMOTE_DIR% || goto default_fail
+goto putty_setup_startup
+
+@REM --------------------------------------------------------------------------------------------
 @REM Setup the Pi to run the script on startup using cron
-@echo Setting up the script to run on startup
-@ssh %ADDRESS% "cd %REMOTE_DIR%/%PY_DIRECTORY% && echo '@reboot python3 -m python.algae.orange_algae_classification' | crontab -" || goto end
+@echo [STEP 3] Setting up the script to run on startup...
+@echo.
 
+:putty_setup_startup
+plink -v -ssh %ADDRESS% -pw %PASSWORD% "cd %REMOTE_DIR%/%PY_DIRECTORY% && echo '@reboot python3 -m python.algae.orange_algae_classification' | crontab -" || (
+    call :putty_fail 
+    goto ssh_setup_startup
+)
+goto job_complete
+
+:ssh_setup_startup
+ssh %ADDRESS% "cd %REMOTE_DIR%/%PY_DIRECTORY% && echo '@reboot python3 -m python.algae.orange_algae_classification' | crontab -" || goto default_fail
+goto job_complete
+
+@REM --------------------------------------------------------------------------------------------
+:job_complete
+@echo.
+@echo Batch job completed.
+goto end
+
+@REM --------------------------------------------------------------------------------------------
 :end
-pause
-exit 0
+@echo.
+@echo.
+@echo Press any key to close the batch.
+@pause >nul
+@exit 0
+
+@REM --------------------------------------------------------------------------------------------
+@REM Error message for PuTTY-related packages fail
+:putty_fail
+@echo.
+@echo Failed to use PuTTY for the task. Will migrate to default powershell commands... Make sure you download the full PuTTY package from here: https://www.chiark.greenend.org.uk/~sgtatham/putty/latest.html
+@echo.
+@exit /b
+
+@REM --------------------------------------------------------------------------------------------
+@REM Error message when default PowerShell commands fail (and ending the batch)
+:default_fail
+@echo.
+@echo Default commands have failed. Ending batch.
+@echo.
+goto end
