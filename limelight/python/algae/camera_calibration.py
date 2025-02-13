bcf5cf51ab064d6d66935bf06d4d240fffa5adf8
WywySenarios <77202595+WywySenarios@users.noreply.github.com>
2025-02-13 10:20:54 -0500

    Camera Color Calibration - Multiple Frames
    Allow camera_calibration script to run the calibration function multiple times.
    
    Slightly changed up the print messages.
    
    I don't yet know if the cv.waitKey()'s inside the calibrate function are redundant.
    
    Added some typing to the program.
    
    The very sad part is that the program can't gracefully quit yet.

--- limelight/python/algae/camera_calibration.py
+++ limelight/python/algae/camera_calibration.py
@@ -1,5 +1,4 @@
 import cv2 as cv
-import time
 import math
 
 """
@@ -12,30 +11,28 @@ WHAT THIS DOES:
 center outside,distance, perfect circle, search for ligthest & darkest, rinse & repeat
 """
 
-mouseX = 0
-mouseY = 0
-mousePressed = False
+mouseX: int = 0
+mouseY: int = 0
+mousePressed: bool = False
 
 # Cal
-originX = 0
-originY = 0
+originX: int = 0
+originY: int = 0
 
-outerX = 0
-outerY = 0
+outerX: int = 0
+outerY: int = 0
 
-def mouse_callback(event, x, y, flags, param):
+def mouse_callback(event, x, y, flags, param) -> None:
     global mouseX
     global mouseY
     global mousePressed
     
-    if event == cv.EVENT_LBUTTONDOWN:  # Left mouse button click
-        print("I've just been clicked! Yay!")
+    if event == cv.EVENT_LBUTTONDOWN:  # Left mouse button click (down)
+        print("Mouse has been clicked: " + str(mouseX) + ", " + str(mouseY))
         mousePressed = True
         mouseX = x
         mouseY = y
-
-def lookForPoint():
-    return [0, 0]
+        # print("Mouse clicked? " + str(mousePressed))
 
 cv.namedWindow("snapshot")
 cv.setMouseCallback("snapshot", mouse_callback)
@@ -43,40 +40,60 @@ cv.setMouseCallback("snapshot", mouse_callback)
 capture = cv.VideoCapture(0) 
 isTrue, frame = capture.read()
 
-while True:
-    if cv.waitKey(1) & 0xFF == ord('a'):
+def calibrate() -> None:
+    global mouseX
+    global mouseY
+    global mousePressed
+    global originX
+    global originY
+    global outerX
+    global outerY
+    global capture
+    global isTrue, frame
+
+    # check if the key pressed was "a",
+    if cv.waitKey(0) & 0xFF == ord('a'):
+        # update the frame
         isTrue, frame = capture.read()
         cv.imshow('snapshot', frame)
-        cv.waitKey(1)
+        cv.waitKey(1) # 
 
-        while True:
-            
+        # make sure that mouse clicks have no effect until a snapshot has been taken (avoid unexpected behaviour)
+        mousePressed = False
+
+        while mousePressed == False:
             cv.imshow('snapshot', frame)
             cv.waitKey(1)
 
-            if mousePressed == True:
-                
-                print(str(mouseX) + ", " + str(mouseY))
+            if mousePressed:
                 originX = mouseX
                 originY = mouseY
+                print("Origin coordinates: (" + str(originX) + ", " + str(originY) + ")")
 
                 mousePressed = False
-                while True:
+                while mousePressed == False:
                     cv.imshow('snapshot', frame)
                     cv.waitKey(1)
-                    if mousePressed == True:
-                        print(str(mouseX) + ", " + str(mouseY))
+                    if mousePressed:
                         outerX = mouseX
                         outerY = mouseY 
-                        mousePressed = False    
+                        print("Outer coordinates: (" + str(originX) + ", " + str(originY) + ")")
+   
+                        mousePressed = False
                         radius = math.sqrt(math.pow(abs(originX-outerX ), 2)+math.pow(abs(originY-outerY ), 2))
                         cv.circle(frame, (originX, originY), int(radius), (0, 0, 255), thickness= 3)
                         cv.imshow("snapshot", frame)
                         cv.waitKey(0)
-              
+                        break
+                break
 
+def main() -> None:
+    while True: 
+        calibrate()
+        print("Done calibrating! Going for another!")
 
-
+if __name__ == "__main__":
+    main()
 
 
         
