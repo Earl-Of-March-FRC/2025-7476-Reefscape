77de0d19a1e5d338dae5980a36db54e4b8b10481
Sam Liu <hello@samliu.dev>
2025-02-23 23:02:45 -0500

    Remove unused imports

--- src/main/java/frc/robot/commands/intake/IntakeSetVelocityPIDCmd.java
+++ src/main/java/frc/robot/commands/intake/IntakeSetVelocityPIDCmd.java
@@ -4,10 +4,7 @@
 
 package frc.robot.commands.intake;
 
-import org.littletonrobotics.junction.Logger;
-
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.intake.IntakeSubsystem;
 
 /**
