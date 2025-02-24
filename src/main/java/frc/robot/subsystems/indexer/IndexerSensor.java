543ac99ee3c374b08e910a484422eba57f248bb3
Sam Liu <hello@samliu.dev>
2025-02-23 22:36:24 -0500

    Change default indexer sensor behaviour

--- src/main/java/frc/robot/subsystems/indexer/IndexerSensor.java
+++ src/main/java/frc/robot/subsystems/indexer/IndexerSensor.java
@@ -5,13 +5,13 @@ package frc.robot.subsystems.indexer;
  * team's confusing switches between a Beam Break and an ultrasonic sensor.
  */
 public interface IndexerSensor {
-    /**
-     * Check if the sensor was triggered by the algae.
-     * 
-     * @return {@code true} if the sensor was triggered or sensor not found.
-     *         {@code false} if not.
-     */
-    default boolean triggered() {
-        return true;
-    }
+  /**
+   * Check if the sensor was triggered by the algae.
+   * 
+   * @return {@code true} if the sensor was triggered or sensor not found.
+   *         {@code false} if not.
+   */
+  default boolean triggered() {
+    return false;
+  }
 }
