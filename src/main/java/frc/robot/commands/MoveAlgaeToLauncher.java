b6c7561fd5185ed095174aa420db29c91f989db6
Wilson <wilsonyu657@gmail.com>
2025-02-25 20:45:45 -0500

    Add intake command

--- src/main/java/frc/robot/commands/MoveAlgaeToLauncher.java
+++ src/main/java/frc/robot/commands/MoveAlgaeToLauncher.java
@@ -7,11 +7,14 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.IndexerConstants;
+import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.LauncherConstants;
 import frc.robot.commands.indexer.IndexerMoveToBeamBreak;
 import frc.robot.commands.indexer.IndexerSetVelocityManualCmd;
+import frc.robot.commands.intake.IntakeSetVelocityManualCmd;
 import frc.robot.commands.launcher.LauncherSetVelocityPIDCmd;
 import frc.robot.subsystems.indexer.Indexer;
+import frc.robot.subsystems.intake.IntakeSubsystem;
 import frc.robot.subsystems.launcher.Launcher;
 
 // NOTE:  Consider using this command inline, rather than writing a subclass.  For more
@@ -21,11 +24,13 @@ public class MoveAlgaeToLauncher extends ParallelCommandGroup {
   /** Creates a new MoveAlgaeToLauncher. */
   public MoveAlgaeToLauncher(
       Launcher launcher,
+      IntakeSubsystem intake,
       Indexer indexer) {
     // Add your commands in the addCommands() call, e.g.
     // addCommands(new FooCommand(), new BarCommand());
     addCommands(
-        new IndexerMoveToBeamBreak(indexer, () -> IndexerConstants.kDirectionConstant),
+        new IndexerMoveToBeamBreak(indexer, () -> IndexerConstants.kDirectionConstant)
+            .raceWith(new IntakeSetVelocityManualCmd(intake, () -> IntakeConstants.kDefaultPercent)),
         new LauncherSetVelocityPIDCmd(launcher, LauncherConstants.kVelocityFront, LauncherConstants.kVelocityBack));
   }
 }
