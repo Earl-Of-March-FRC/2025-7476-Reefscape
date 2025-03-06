64515d7f1e973015224fa760b0e8303fe74d9257
Angela Zheng <angelazheng2007@gmail.com>
2025-03-05 22:58:08 -0500

    Added alliance dependent pose for launch shot pathfinding

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -126,7 +126,8 @@ public final class Constants {
         kMaxAccelerationMetersPerSecondSquared, kMaxAngularSpeedRadiansPerSecond,
         kMaxAngularAccelerationRadiansPerSecondSquared);
 
-    public static final Pose2d kLaunchPose = new Pose2d(new Translation2d(10.53, 2.68), new Rotation2d(0));
+    public static final Pose2d kLaunchPoseBlue = new Pose2d(new Translation2d(7.02, 5.37), new Rotation2d(0));
+    public static final Pose2d kLaunchPoseRed = new Pose2d(new Translation2d(10.53, 2.68), new Rotation2d(0));
 
   }
 
