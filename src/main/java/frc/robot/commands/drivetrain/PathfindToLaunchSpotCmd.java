b2d1af6fe6370e6e855a3dfe1c4d464fefe05e41
Sam Liu <hello@samliu.dev>
2025-03-07 15:29:15 -0500

    Change auto speed

--- src/main/java/frc/robot/commands/drivetrain/PathfindToLaunchSpotCmd.java
+++ src/main/java/frc/robot/commands/drivetrain/PathfindToLaunchSpotCmd.java
@@ -11,21 +11,12 @@ import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.AutoConstants;
+import frc.robot.Constants.DriveConstants;
 
 public class PathfindToLaunchSpotCmd extends SequentialCommandGroup {
   /** Creates a new PathfindToLaunchSpotCmd. */
-  public PathfindToLaunchSpotCmd() {
-
-    // Determine launch pose to use based on alliance
-    Pose2d launchPose;
-
-    if (DriverStation.getAlliance().get() == Alliance.Blue) {
-      launchPose = AutoConstants.kLaunchPoseBlue;
-    } else {
-      launchPose = AutoConstants.kLaunchPoseRed;
-    }
-
+  public PathfindToLaunchSpotCmd(Pose2d launchPose) {
     addCommands(
-        AutoBuilder.pathfindToPose(launchPose, AutoConstants.kPathfindingConstraints));
+        AutoBuilder.pathfindToPose(launchPose, DriveConstants.kPathfindingConstraints));
   }
 }
