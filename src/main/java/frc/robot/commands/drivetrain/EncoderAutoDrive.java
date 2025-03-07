1ed87520408e100b17728e53649b98e6e6c7946b
Wilson <wilsonyu657@gmail.com>
2025-03-07 15:21:48 -0500

    Remove unused import

--- src/main/java/frc/robot/commands/drivetrain/EncoderAutoDrive.java
+++ src/main/java/frc/robot/commands/drivetrain/EncoderAutoDrive.java
@@ -4,8 +4,6 @@
 
 package frc.robot.commands.drivetrain;
 
-import java.util.Arrays;
-
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj2.command.Command;
