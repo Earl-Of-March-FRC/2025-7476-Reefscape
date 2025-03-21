ac83cd7c7c135d35bfbe510a6c1847bc270563aa
Sam Liu <hello@samliu.dev>
2025-03-20 21:36:43 -0400

    Update swerve module sim to match behaviour with real swerve module Added optimization of wheel turning

--- src/main/java/frc/robot/subsystems/drivetrain/SwerveModuleSim.java
+++ src/main/java/frc/robot/subsystems/drivetrain/SwerveModuleSim.java
@@ -2,14 +2,13 @@ package frc.robot.subsystems.drivetrain;
 
 import org.ironmaple.simulation.drivesims.SwerveModuleSimulation;
 import org.ironmaple.simulation.motorsims.SimulatedMotorController;
-import org.littletonrobotics.junction.Logger;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.units.Units;
-import edu.wpi.first.units.measure.Angle;
 import frc.robot.Constants.ModuleConstants;
 
 public class SwerveModuleSim implements SwerveModule {
@@ -20,6 +19,8 @@ public class SwerveModuleSim implements SwerveModule {
   private final PIDController drivePID;
   private final SimpleMotorFeedforward driveFeedforward;
   private final PIDController turnController;
+
+  private double m_chassisAngularOffset = 0;
 
   public SwerveModuleSim(SwerveModuleSimulation moduleSim) {
     moduleSimulation = moduleSim;
@@ -52,14 +53,23 @@ public class SwerveModuleSim implements SwerveModule {
   }
 
   @Override
-  public void setDesiredState(SwerveModuleState state) {
+  public void setDesiredState(SwerveModuleState desiredState) {
+    // Apply chassis angular offset to the desired state.
+    SwerveModuleState correctedDesiredState = new SwerveModuleState();
+    correctedDesiredState.speedMetersPerSecond = desiredState.speedMetersPerSecond;
+    correctedDesiredState.angle = desiredState.angle.plus(Rotation2d.fromRadians(m_chassisAngularOffset));
+
+    // Optimize the reference state to avoid spinning further than 90 degrees.
+    correctedDesiredState.optimize(moduleSimulation.getSteerAbsoluteFacing());
+
     driveMotor.requestVoltage(
         Units.Volts.of(driveFeedforward.calculate(
-            state.speedMetersPerSecond / (ModuleConstants.kWheelDiameterMeters / 2)))
+            correctedDesiredState.speedMetersPerSecond / (ModuleConstants.kWheelDiameterMeters / 2)))
             .plus(
                 Units.Volts.of(drivePID.calculate(moduleSimulation.getDriveWheelFinalSpeed().in(Units.RadiansPerSecond),
-                    state.speedMetersPerSecond / (ModuleConstants.kWheelDiameterMeters / 2)))));
+                    correctedDesiredState.speedMetersPerSecond / (ModuleConstants.kWheelDiameterMeters / 2)))));
     turnMotor.requestVoltage(Units.Volts.of(
-        turnController.calculate(moduleSimulation.getSteerAbsoluteFacing().getRadians(), state.angle.getRadians())));
+        turnController.calculate(moduleSimulation.getSteerAbsoluteFacing().getRadians(),
+            correctedDesiredState.angle.getRadians())));
   }
 }
