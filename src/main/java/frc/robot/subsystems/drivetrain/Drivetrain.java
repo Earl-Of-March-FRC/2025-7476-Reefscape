5a7874d3315dc734aed68a8e8415d5dbbeb12fe8
EomLions <eomlions@gmail.com>
2025-03-22 12:39:52 -0400

    Adjust vision std deviations

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -91,7 +91,7 @@ public class Drivetrain extends SubsystemBase {
       },
       new Pose2d(0, 0, new Rotation2d()),
       VecBuilder.fill(0.1, 0.1, 0.1),
-      VecBuilder.fill(0.7, 0.7, 0.7));
+      VecBuilder.fill(0.5, 0.5, 0.5));
 
   SwerveDrivePoseEstimator visionlessOdometry = new SwerveDrivePoseEstimator(
       DriveConstants.kDriveKinematics,
