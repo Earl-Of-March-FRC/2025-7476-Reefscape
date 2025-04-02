04b54a9c3ceccc5ed29bb7b55cc59d748d835c25
Wilson <wilsonyu657@gmail.com>
2025-04-02 19:49:10 -0400

    Stop clearing offset when launcher is stopped

--- src/main/java/frc/robot/subsystems/launcher/Launcher.java
+++ src/main/java/frc/robot/subsystems/launcher/Launcher.java
@@ -196,7 +196,6 @@ public class Launcher extends SubsystemBase {
    * Stops the launcher motors.
    */
   public void stopLauncher() {
-    setReferenceVelocityOffset(0);
     setReferenceVelocity(0);
   }
 
